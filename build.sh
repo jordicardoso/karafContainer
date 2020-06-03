bb7f8d3d3e0726d092ed5a413ad5d2eb2aaabbd6
jordi <jordi.cardoso@bonpreu.cat>
2020-06-03 11:19:25 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ Usage:
   build.sh --template <name> 
   build.sh --help
   templates: ${TEMPLATES} 
-  camel versions: 2.20.0-4, 2.21.0-5, 2.22.0-5, 2.23.0-4, 2.24.0-3, 2.25.0-1, 3.0.0-1, 3.1.0, 3.2.0, 3.3.0
-  karaf versions java8:  4.1.7
+  camel version: 2.25.1
   karaf versions java11: 4.2.8
 HERE
   exit 1
